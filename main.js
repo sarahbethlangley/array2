5c45df78a8d81fda0c1f32c4fbd1c922f100634b
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-03-22 13:44:34 -0500

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+const yellowFruit = "Banana";
+const orangeFruit = "Orange";
+const redFruit = "Apple";
+const greenFruit = "Watermelon";
+const blueFruit = "Blueberry";
+
+const fruits = [yellowFruit, orangeFruit, redFruit, greenFruit, blueFruit];
+
+console.log(fruits);
